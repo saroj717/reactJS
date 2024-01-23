8b34493fe8101a3cc08fed11a55adcd70df4bb5b
Saroj Shrestha <saroj717@yahoo.com>
2024-01-23 11:36:41 -0700

    commits

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ const element = React.createElement (
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot (container);
 root.render (element);
+
+const container2 = document.getElementById('root2');
+const root2 = ReactDOM.createRoot(container2);
+root2.render(element);
+
+document.querySelectorAll('.button')
+  .forEach(domContainer => {
+    const messageId = parseInt(domContainer.dataset.messageId);
+
+    const root = ReactDOM.createRoot(domContainer);
+
+    root.render(
+      React.createElement(
+        'button',
+        {
+          onClick: () => {console.log(`liked message: ${messageId}`);}
+        },
+        'Like'
+      )
+    )
+
+  });
